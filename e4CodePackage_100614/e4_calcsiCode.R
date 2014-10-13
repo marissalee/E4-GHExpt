a2fa391b066c15bb5250e7b14017c74c88d5c0cd
Marissa Lee <marissaruthlee@gmail.com>
2014-10-13 14:02:39 -0400

    Changed the calculation of si.  It used to be simply a measure of the difference in each soil measurement in the presence or absence of M.v. at each M.v. density treatment.  Now, the difference is relative to 1) empty pots and 2) non-M.v. fully planted pots.  For comptrt = N, #2 is always NA.

--- e4CodePackage_100614/e4_calcsiCode.R
+++ e4CodePackage_100614/e4_calcsiCode.R
@@ -1,20 +1,23 @@
 #e4_calcsiCode.R
 #Calculate the impact of invader presence on soil properties (Si1 - Si1) and create new df for this info  
 
-##Start by creating a column to subset the data by neighbor treatment.  
-data$ss1 <- rep(NA,dim(data)[1]) # create the new column to hold factors to separate rows by neighbor treatment that excludes 100% Mivi but includes 0% Mivi
-data[data$type=='Empty' | data$type=='CompEmpty', 'ss1'] <- 'E' # populate that column
-data[data$type=='Pavi' | data$type=='CompPavi', 'ss1'] <- 'P'
-data[data$type=='Sobi' | data$type=='CompSobi', 'ss1'] <- 'S'
-data[data$type=='Mivi', 'ss1'] <- 'Mv100'
+#str(data)
+
+##Start by creating 3 cols, each defines the data subset for each comptrt: N, P, S.
+data$ssN<-rep(NA, dim(data)[1]) # holds N filter
+data$ssP<-rep(NA, dim(data)[1]) # holds P filter
+data$ssS<-rep(NA, dim(data)[1]) # holds S filter
+##Populate columns with a key to the rows they need
+data[data$type=='Empty' | data$type=='CompEmpty', 'ssN'] <- 1
+data[data$type=='Empty' | data$type=='Pavi' | data$type=='CompPavi', 'ssP'] <- 1
+data[data$type=='Empty' | data$type=='Sobi' | data$type=='CompSobi', 'ssS'] <- 1
 #View(data)
 
-
-#+ Within each bk * neighbor treatment, subtract the Si0 from each Si1. 
+#+ Within each bk * neighbor treatment, calculate the deltaS relative to (1) empty pots and (2) full, non-mv pots
 BK <-unique(data$bk) # list of bks to loop through
 e<-0
 
-SS1 <- unique(data$ss1)[-1] # list of ss1s to loop through, exclude Mv100
+SS <- c('ssN','ssP','ssS')
 f<-0
 
 SCOLS <- colnames(data)[11:17] # list of S columns to loop through
@@ -28,51 +31,82 @@ mylist<-list() # to store results
 mylist.check<-list() # to check the looping stuff
 
 # define column names
-storecols<-c('s1pid', 's0pid', 'scol', 's1', 's0', 'bk','julydate','type','comptrt','mvtrt','mivi','compabund','total','ss1')
-checkcols<-c('iter','s1row','s1col','s0row','s0col')
+storecols<-c('sFpid', 's0Epid', 's0Ppid',
+             'scol', 
+             'sF', 's0E', 's0P',
+             'bk','julydate','type','comptrt','mvtrt','mivi','compabund','total')
+checkcols<-c('iter',
+             'sFrow','sFcol',
+             's0Erow','s0Ecol',
+             's0Prow','s0Pcol')
 
-for (e in 1:length(BK)){ # 1. loop through each bk, for the current bk...
-  for (f in 1:length(SS1)){ # 2. loop through each ss1, for the current ss1....
-    ss<-data[data$bk == BK[e] & data$ss1 == SS1[f], ] # 3. Subset data by bk * ss1
-    for (g in 1:length(SCOLS)){ # 2. loop through each S column (nhdi, nodi, etc), for the current S column....
-      for (h in 1:length(MVTRT)){ # 3. loop through each S column value associated with mvtrt 1,2,4,5, for the current value...
+# 1. loop through each bk, for the current bk...
+for (e in 1:length(BK)){ 
+  # 1a. subset data by the current BK
+  sub1<-data[data$bk == BK[e],] 
+  
+  # 2. loop through each SS, for the current SS...
+  for (f in 1:length(SS)){ 
+    # 2a. subset sub1 by the current SS
+    ssfilter<-which(sub1[,SS[f]] == 1)
+    sub2<-sub1[ssfilter,]
+    
+    # 3. loop through each S column (nhdi, nodi, etc), for the current S column....
+    for (g in 1:length(SCOLS)){ 
+      
+      # 4. loop through each S column value associated with mvtrt 1,2,4,5, for the current value...
+      for (h in 1:length(MVTRT)){ 
         
-        # check the looping stuff
-        iter <- paste(BK[e], SS1[f], SCOLS[g], MVTRT[h], sep='_')
-        s1dim <- dim(ss[ss$mvtrt == MVTRT[h], ])
-        s0dim <- dim(ss[ss$mvtrt == 0, ])
-        mydata.check <- c(iter, s1dim, s0dim) # make a vector to store that data
+        # 5. check the looping stuff
+        iter <- paste(BK[e], SS[f], SCOLS[g], MVTRT[h], sep='_')
+        
+        sFrow<-sub2[sub2$mvtrt == MVTRT[h], ]
+        sFdim<-dim(sFrow)
+        
+        s0Erow <- sub2[sub2$mvtrt == 0 & sub2$type == 'Empty', ]
+        s0Edim <- dim(s0Erow)
+        
+        s0Prow <- sub2[sub2$mvtrt == 0 & sub2$type != 'Empty', ]
+        s0Pdim <- dim(s0Prow)
+        
+        mydata.check <- c(iter, sFdim, s0Edim, s0Pdim) # make a vector to store that data
         mylist.check [[as.character(iter)]] <- mydata.check # put that vector into the initalized list and at the same time, give it a unique name (as.character(iter))
         
-        # 4. assign this info:
+        # 6. assign this info:
         
-        if(s1dim[1] == 1){
-          # 4.1 potid for Si1 and associated info
-          s1pid<-ss[ss$mvtrt == MVTRT[h], 'potid']
-          s1info<-ss[ss$mvtrt == MVTRT[h], c('bk','julydate','type','comptrt','mvtrt','mivi','compabund','total','ss1')] # 8 elements
-          
-          # 4.2 s1 value associated with mvtrt (1-5)
-          s1 <- ss[ss$mvtrt == MVTRT[h], SCOLS[g]]
+        # 6.1 sF potid, sF info, sF value
+        if(sFdim[1] == 1){
+           
+           sFpid <- sFrow[ ,'potid']
+           sFinfo <- sFrow[, c('bk','julydate','type','comptrt','mvtrt','mivi','compabund','total')] # 8 elements
+           sF <- sFrow[ , SCOLS[g]]
+         } else{
+           sFpid <- NA
+           sFinfo <- rep(NA,8)
+           sF<-NA
+         }
+        
+        # 6.2 s0E potid, s0E value
+        s0Epid<-s0Erow[, 'potid']
+        s0E <- s0Erow[, SCOLS[g]]
+        
+        # 6.3 s0P potid, s0P value
+        if(s0Pdim[1] == 1){
+          s0Ppid <- s0Prow[, 'potid']
+          s0P <- s0Prow[, SCOLS[g]]
         } else{
-          # 4.1 potid for Si1
-          s1pid<-NA
-          s1info<-rep(NA,8)
-          
-          # 4.2 s1 value associated with mvtrt (1-5)
-          s1<-NA
+          s0Ppid <- NA
+          s0P <- NA
         }
         
-        # 4.3 potid Si0 and comma separated Si1,Si0 potids
-        s0pid<-ss[ss$mvtrt==0, 'potid']
-        
-        # 4.4 S column name (e.g. nhdi)
+        # 6.4 S column name (e.g. nhdi)
         scol<-SCOLS[g] 
         
-        # 4.5 s0 value associated with mvtrt = 0, calc s1 - s0
-        s0 <- ss[ss$mvtrt == 0, SCOLS[g]]
-        
-        # 5. store the assigned info in a row, add the new row to previous rows
-        mydata <- c(s1pid, s0pid, scol, s1, s0, s1info) # 14 cells, make a vector to store that data
+        # 7. store the assigned info in a row, add the new row to previous rows
+        mydata <- c(sFpid, s0Epid, s0Ppid, # 3 elements
+                    scol, # 1 element
+                    sF, s0E, s0P, # 3 elements
+                    sFinfo) # 8 elements
         mylist [[as.character(iter)]] <- mydata # put that vector into the initalized list and at the same time, give it a unique name (as.character(iter))
         
       }
@@ -80,15 +114,26 @@ for (e in 1:length(BK)){ # 1. loop through each bk, for the current bk...
   }
 }
 
-#Look at the dimensions of s1 and s0 through each unique iteration to make sure the loops worked
+#Look at the dimensions of sF, s0E, and s0P through each unique iteration to make sure the loops worked
 finaldata.check <- do.call(rbind, mylist.check)
 colnames(finaldata.check) <- checkcols
 
-#unique(finaldata.check[,'s1row']) # there are some iterations with 0 s1rows
-messedup<-which(finaldata.check[,'s1row']==0)
-#finaldata.check[messedup,] 
+#check sF
+unique(finaldata.check[,'sFrow']) # there are some iterations with 0 rows
+messedup<-which(finaldata.check[,'sFrow']==0)
+finaldata.check[messedup,] # bk 5, ssN, mvtrt 5
 
-#ok, so I am adding an IF statement that says if s1=1, then do the normal stuff, if not, then (1) insert NA in s1 and s1-s0 cells and (2) print a warning.
+#check s0E
+unique(finaldata.check[,'s0Erow']) # looks good
+
+#check s0P
+unique(finaldata.check[,'s0Prow']) # there are some iterations with 0 rows
+messedup<-which(finaldata.check[,'s0Prow']==0)
+finaldata.check[messedup,] # all ssN
+
+#ok, I added IF statements that says if there is no sF or s0P value, then do the normal stuff, if not, then insert NAs
+
+##
 #now, look at the results
 finaldata <- do.call(rbind, mylist)
 colnames(finaldata) <- storecols
@@ -103,18 +148,21 @@ colnames(df)<-storecols
 #str(df)
 
 #convert potids from character strings to numbers
-df$s1pid<-as.numeric(df$s1pid)
-df$s0pid<-as.numeric(df$s0pid)
+df$sFpid<-as.numeric(df$sFpid)
+df$s0Epid<-as.numeric(df$s0Epid)
+df$s0Ppid<-as.numeric(df$s0Ppid)
 df$bk<-as.numeric(df$bk)
 df$julydate<-as.numeric(df$julydate)
 #str(df)
 
 #convert each to the appropriate form
-df$s1pid<-as.factor(df$s1pid)
-df$s0pid<-as.factor(df$s0pid)
+df$sFpid<-as.factor(df$sFpid)
+df$s0Epid<-as.factor(df$s0Epid)
+df$s0Ppid<-as.factor(df$s0Ppid)
 df$scol<-as.factor(df$scol)
-df$s1<-as.numeric(df$s1)
-df$s0<-as.numeric(df$s0)
+df$sF<-as.numeric(df$sF)
+df$s0E<-as.numeric(df$s0E)
+df$s0P<-as.numeric(df$s0P)
 df$bk<-as.factor(df$bk)
 df$julydate<-as.factor(df$julydate)
 df$type<-as.factor(df$type)
@@ -123,17 +171,20 @@ df$mvtrt<-as.factor(df$mvtrt)
 df$mivi<-as.numeric(df$mivi)
 df$compabund<-as.numeric(df$compabund)
 df$total<-as.numeric(df$total)
-df$ss1<-as.factor(df$ss1)
 #str(df)
-
 #head(df) #looks good
 
-#+ Order df by s1pid and remove rows with s1pid == NA
-df2<-df[!is.na(df$s1pid),]
+#+ Remove rows with sFpid == NA
+df2<-df[!is.na(df$sFpid),]
 #head(df2)
 
-#+ Finally, subtract s1 - s0, populate s1s0 column with the resulting values.  Re-name the dataframe.
-df2$s1s0 <- df2$s1 - df2$s0
+#+ Calculate the deltas: delta.sE and delta.sP
+# delta.sE
+df2$delta.sE <- df2$sF - df2$s0E
+# delta.sP
+df2$delta.sP <- df2$sF - df2$s0P
+
+#+ Update the name of this data.frame
 datas <- df2
 #View(datas)
 
